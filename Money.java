5e8340abf7954631b056ad0f3982b7ee23fabb50
AastikGautam <34084056+AastikGautam@users.noreply.github.com>
2018-02-10 16:09:01 +0530

    Add files via upload

---
+++ Money.java
@@ -0,0 +1,35 @@
+	import java.text.NumberFormat;
+
+
+
+	public class Money {
+
+		
+
+		public static void main(String[] args) {
+
+			NumberFormat money = NumberFormat.getCurrencyInstance();
+
+			NumberFormat percent = NumberFormat.getPercentInstance();
+
+			
+
+			double initialValue, total, interestRate;
+
+			
+
+			initialValue = 5000;
+
+			interestRate = .20;
+
+			
+
+			total = initialValue + initialValue*interestRate;
+
+			
+
+			System.out.println(money.format(total));
+
+		}
+
+	}
